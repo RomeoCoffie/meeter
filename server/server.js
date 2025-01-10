cfb5c87250f38e3164d551c438acaf80ac21f387
RomeoCoffie <romeocoffie@dextsolutions.com>
2025-01-10 13:25:59 +0000

    render debugging

--- server/server.js
+++ server/server.js
@@ -29,9 +29,9 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // Routes
-app.use('/api/auth', require('./routes/auth.routes'));
-app.use('/api/meetings', require('./routes/meeting.routes'));
-app.use('/api/users', require('./routes/user.routes'));
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/meetings', require('./routes/meeting.routes'));
+app.use('/users', require('./routes/user.routes'));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
